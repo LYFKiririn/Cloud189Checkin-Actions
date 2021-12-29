509cf3ea78d34d43d157115ef2eee8ddbf2b01d8
Kiririn <41487236+LYFKiririn@users.noreply.github.com>
2021-12-29 14:03:56 +0800

    Update C189Checkin.py

--- C189Checkin.py
+++ C189Checkin.py
@@ -10,7 +10,7 @@ if(username == "" or password == ""):
     username = input("账号：")
     print(username[:3] + '****')
     password = input("密码：")
-    print('****')
+    print('*******')
 
 def main():
     #登录
